e5f74cafff2b5d4ff1c067fd6ebeb43ac85e86e3
facetint <fatmanurcetintas7@gmail.com>
2025-12-16 18:26:10 +0300

    feat(router): extend app routing with home and dashboard pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login/LoginPage'
+import HomePage from './pages/Home/HomePage'
 import './App.css'
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/dashboard" element={<HomePage />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/register" element={<Login />} />
       </Routes>
