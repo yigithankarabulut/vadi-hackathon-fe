e468d04923dc9646eb3c2f7c8a4cda44349bd493
facetint <fatmanurcetintas7@gmail.com>
2025-12-17 07:42:42 +0300

    feat(auth): integrate user context into routing layer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import Login from './pages/Login/LoginPage'
 import HomePage from './pages/Home/HomePage'
 import './App.css'
+import { useUser } from './context/UserContext'
 
 function App() {
+  const { isLoggedIn } = useUser();
+  
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Login />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/" element={<Navigate to="/login" replace />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Login />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+        </Routes>
+      </Router>
+
   )
 }
 
